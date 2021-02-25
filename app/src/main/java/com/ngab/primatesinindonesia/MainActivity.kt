c4c64a70ef396c54a528cb63b11e1fa85e7a29ef
willyishmael <wtangka22@gmail.com>
2021-02-25 15:22:52 +0800

    just a little change

--- app/src/main/java/com/ngab/primatesinindonesia/MainActivity.kt
+++ app/src/main/java/com/ngab/primatesinindonesia/MainActivity.kt
@@ -11,8 +11,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root
+        setContentView(binding.root)
 
-        )
+
     }
 }
