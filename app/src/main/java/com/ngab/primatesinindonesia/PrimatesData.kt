46b993ea5af7fc5fc89b5ae0212a50771843a2ee
willyishmael <wtangka22@gmail.com>
2021-03-01 22:35:45 +0800

    add primateImages array

--- app/src/main/java/com/ngab/primatesinindonesia/PrimatesData.kt
+++ app/src/main/java/com/ngab/primatesinindonesia/PrimatesData.kt
@@ -12,5 +12,19 @@ object PrimatesData {
             "Tarsius Bangka",
             "Tarsius Siau"
     )
+
+    
+    private val primateImages = intArrayOf(
+            R.drawable.beruk_mentawai,
+            R.drawable.kukang_jawa,
+            R.drawable.lutung_kedih,
+            R.drawable.monyet_ekor_panjang,
+            R.drawable.owa_lengan_putih,
+            R.drawable.siamang,
+            R.drawable.simpai,
+            R.drawable.surili,
+            R.drawable.tarsius_bangka,
+            R.drawable.tarsius_siau
+    )
 }
 
