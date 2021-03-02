c1fc2d6e2308eaf2f1621976ef83b194cd2b19de
willyishmael <wtangka22@gmail.com>
2021-03-02 13:38:14 +0800

    adapter

--- app/src/main/java/com/ngab/primatesinindonesia/ListPrimateAdapter.kt
+++ app/src/main/java/com/ngab/primatesinindonesia/ListPrimateAdapter.kt
@@ -1,4 +1,27 @@
 package com.ngab.primatesinindonesia
 
-class ListPrimateAdapter {
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ListPrimateAdapter(private val listPrimate: ArrayList<Primate>) : RecyclerView.Adapter<ListPrimateAdapter.ListViewHolder>() {
+    class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var tvName: TextView = itemView.findViewById(R.id.tv_item_name)
+        var tvDetail: TextView = itemView.findViewById(R.id.tv_item_detail)
+        var imgPhoto: ImageView = itemView.findViewById(R.id.img_item_photo)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
 }
