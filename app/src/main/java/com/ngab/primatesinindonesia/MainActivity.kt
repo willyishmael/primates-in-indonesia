2e8fbc9a785581523eb587ed41cc2cd55bb1da37
willyishmael <wtangka22@gmail.com>
2021-03-05 19:47:32 +0800

    initialize rvPrimates on MainActivity

--- app/src/main/java/com/ngab/primatesinindonesia/MainActivity.kt
+++ app/src/main/java/com/ngab/primatesinindonesia/MainActivity.kt
@@ -2,17 +2,18 @@ package com.ngab.primatesinindonesia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 import com.ngab.primatesinindonesia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding:ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
+        binding.rvPrimates.setHasFixedSize(true)
     }
 }
