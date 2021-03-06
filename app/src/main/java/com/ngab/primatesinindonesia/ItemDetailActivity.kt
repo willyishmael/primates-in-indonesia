d27f69eb39019ec954b9b17ed5072aab717e09a9
willyishmael <wtangka22@gmail.com>
2021-03-06 17:26:42 +0800

    no message

--- app/src/main/java/com/ngab/primatesinindonesia/ItemDetailActivity.kt
+++ app/src/main/java/com/ngab/primatesinindonesia/ItemDetailActivity.kt
@@ -8,9 +8,15 @@ class ItemDetailActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityItemDetailBinding
 
+    companion object {
+        const val EXTRA_ITEM_POSITION = "extra_item_position"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityItemDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val primate = intent.getIntExtra(EXTRA_ITEM_POSITION, 0)
     }
 }
